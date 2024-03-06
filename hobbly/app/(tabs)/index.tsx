7c28afb408399e395c4a10e2024e093c816c4ada
Kuba <01169177@pw.edu.pl>
2024-03-06 20:44:08 +0100

    clerk auth, list, details page, reanimated

--- hobbly/app/(tabs)/index.tsx
+++ hobbly/app/(tabs)/index.tsx
@@ -1,19 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { View } from 'react-native'
+import React, { useMemo, useState } from 'react'
+import { Stack } from 'expo-router'
+import ExploreHeader from '@/components/ExploreHeader'
+import Listings from '@/components/Listings'
+import listingsData from '@/assets/data/airbnb-listings.json';
 
 const Page = () => {
+    const [category, setCategory] = useState('Tine homes');
+    const items = useMemo(() => listingsData as any, []);
+
+    const onDataChanged = (category: string) => {
+        setCategory(category);
+    };
+
     return (
-        <View>
-            <Link href={"/(modals)/login"}>
-                Login
-            </Link>
-            <Link href={"/(modals)/booking"}>
-                Bookings
-            </Link>
-            <Link href={"/listing/123"}>
-                Listing Details
-            </Link>
+        <View style={{ flex: 1, marginTop: 130 }}>
+            <Stack.Screen
+                options={{
+                    header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+                }}
+            />
+            <Listings listings={items} category={category} />
         </View>
     )
 }
--- hobbly/app/_layout.tsx
+++ hobbly/app/_layout.tsx
@@ -2,9 +2,29 @@ import { useFonts } from 'expo-font';
 import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
 import { TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -41,11 +61,23 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache} >
+      <RootLayoutNav />
+    </ClerkProvider >
+  );
+
 }
 
 function RootLayoutNav() {
   const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login');
+    }
+  }, [isLoaded])
 
   return (
     <Stack>
@@ -65,7 +97,7 @@ function RootLayoutNav() {
           )
         }}
       />
-      <Stack.Screen name='listing/[id]' options={{ headerTitle: '' }} />
+      <Stack.Screen name='listing/[id]' options={{ headerTitle: '', headerTransparent: true }} />
       <Stack.Screen name='(modals)/booking'
         options={{
           presentation: 'transparentModal',
